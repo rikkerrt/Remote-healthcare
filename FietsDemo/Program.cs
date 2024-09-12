4bca61c337981405a91faa37dd997c78fe1f51ab
Rick <rrhvanhoof@gmail.com>
2024-09-12 14:36:53 +0200

    Auto stash before merge of "main" and "origin/main"

--- FietsDemo/Program.cs
+++ FietsDemo/Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -130,4 +131,137 @@ namespace FietsDemo
 
         }
     }
+=======
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Avans.TI.BLE;
+
+namespace FietsDemo
+{
+    class Program
+    {
+        private static double Distance = 0;
+        private static int DistanceCount = 0;
+        private static int lastDistanceValue;
+        
+        private static double Duration = 0;
+        private static int DurationCount = 0;
+        private static int lastDurationValue;
+        static async Task Main(string[] args)
+        {
+            int errorCode = 0;
+            BLE bleBike = new BLE();
+            BLE bleHeart = new BLE();
+            Thread.Sleep(1000); // We need some time to list available devices
+
+            // List available devices
+            List<String> bleBikeList = bleBike.ListDevices();
+            Console.WriteLine("Devices found: ");
+            foreach (var name in bleBikeList)
+            {
+                Console.WriteLine($"Device: {name}");
+            }
+
+            // Connecting
+            errorCode = errorCode = await bleBike.OpenDevice("Tacx Flux 01249");
+            // __TODO__ Error check
+
+            var services = bleBike.GetServices;
+            foreach(var service in services)
+            {
+                Console.WriteLine($"Service: {service.Name}");
+            }
+
+            // Set service
+            errorCode = await bleBike.SetService("6e40fec1-b5a3-f393-e0a9-e50e24dcca9e");
+            // __TODO__ error check
+
+            // Subscribe
+            bleBike.SubscriptionValueChanged += BleBike_SubscriptionValueChanged;
+            errorCode = await bleBike.SubscribeToCharacteristic("6e40fec2-b5a3-f393-e0a9-e50e24dcca9e");
+
+            // Heart rate
+            errorCode =  await bleHeart.OpenDevice("Decathlon Dual HR");
+
+            await bleHeart.SetService("HeartRate");
+
+            bleHeart.SubscriptionValueChanged += BleBike_SubscriptionValueChanged;
+            await bleHeart.SubscribeToCharacteristic("HeartRateMeasurement");
+             
+
+            Console.Read();
+        }
+
+        private static void BleBike_SubscriptionValueChanged(object sender, BLESubscriptionValueChangedEventArgs e)
+        {
+            //Console.WriteLine("Received from {0}: {1}, {2}", e.ServiceName,
+            //    BitConverter.ToString(e.Data).Replace("-", " "),
+                //Encoding.UTF8.GetString(e.Data));
+            String filter = BitConverter.ToString(e.Data).Replace("-", " ");
+
+            //if (filter.Substring(12, 2).Equals("10")) {
+                //Console.WriteLine("Afstand: " + GetDistance("aa"));
+                //Console.WriteLine("Snelheid: " + GetSpeed(filter.Substring(24, 2), filter.Substring(27, 2)) + " Km/h");
+                //Console.WriteLine("Tijd: " + GetDuration("ab" + " S"));
+
+                double Speed = GetSpeed(filter.Substring(24, 2), filter.Substring(27, 2));
+                double Duration = GetDuration(filter.Substring(18,2));
+                double Distance = GetDistance(filter.Substring(21,2));
+
+                Console.WriteLine("Tijdsduur: "+ Duration);
+                Console.WriteLine("Afstand: "+Distance);   
+                Console.WriteLine("Snelheid: "+Speed);
+
+                   }
+
+        private static int HexToDecimal(string hexValue)
+        {
+            int decValue = int.Parse(hexValue, System.Globalization.NumberStyles.HexNumber);
+            return decValue;
+
+        }
+
+        private static double GetSpeed(string LSB, string MSB)
+        {
+            string TotalHexValue = MSB + LSB;
+            int DecValue = HexToDecimal(TotalHexValue);
+            Double SpeedInKmH = (DecValue * 0.001)*3.6;
+            return SpeedInKmH;
+        }
+
+        public static double GetDistance(string distanceValue)
+        {
+
+            int decValue = HexToDecimal(distanceValue);
+            if (decValue < lastDistanceValue)
+            {
+                DistanceCount = DistanceCount + 1;
+            }
+            Distance = decValue + (DistanceCount * 255);
+            lastDistanceValue = decValue;
+
+            return Distance;
+
+        }
+        
+        public static double GetDuration(string HexDurationValue)
+        {
+
+            int decValue = HexToDecimal(HexDurationValue);
+            if (decValue < lastDurationValue)
+            {
+                DurationCount  = DurationCount + 1;
+            }
+            Duration = decValue + (DurationCount * 255);
+            lastDurationValue = decValue;
+
+            return Duration/4;
+
+        }
+    }
+>>>>>>> Stashed changes
 }
