6bc8ed2f8acd147e444cb4aad33f4980e28820ee
Pieter <pieter.degraaff@icloud.com>
2024-09-12 14:05:09 +0200

    juiste code toegevoegd

--- Remote-healthcare/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Remote_healthcare;
-
-Console.WriteLine("Hello, World!");
-BikeSimulator bike = new BikeSimulator();
-
-
-Console.WriteLine(bike.lol());
