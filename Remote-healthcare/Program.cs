2afb117b49467a35f7d86c661acee5279e5421e3
Falcon.G2 <ctw.vanderpluijm@student.avans.nl>
2024-09-12 14:12:05 +0200

    Revert "juiste code toegevoegd"
    This reverts commit 6bc8ed2f8acd147e444cb4aad33f4980e28820ee.

---
+++ Remote-healthcare/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Remote_healthcare;
+
+Console.WriteLine("Hello, World!");
+BikeSimulator bike = new BikeSimulator();
+
+
+Console.WriteLine(bike.lol());
