b98c6f6913727c86eb370d5ede7331616fd1d3df
Rick <rrhvanhoof@gmail.com>
2024-09-26 15:03:01 +0200

    Complete file fix
    files veranderd zodat het weer werkt

--- ClientProgram/ClientProgram/TestClient.cs
+++ ClientProgram/ClientProgram/TestClient.cs
@@ -1,17 +1,17 @@
 ﻿
-using System.Net.Sockets;
-using System.Net;
-using System.Text;
+//using System.Net.Sockets;
+//using System.Net;
+//using System.Text;
 
-var ipEndPoint = new IPEndPoint(IPAddress.Any, 13);
+//var ipEndPoint = new IPEndPoint(IPAddress.Any, 13);
 
-using TcpClient client = new();
-await client.ConnectAsync(ipEndPoint);
-await using NetworkStream stream = client.GetStream();
+//using TcpClient client = new();
+//await client.ConnectAsync(ipEndPoint);
+//await using NetworkStream stream = client.GetStream();
 
-var buffer = new byte[1_024];
-int received = await stream.ReadAsync(buffer);
+//var buffer = new byte[1_024];
+//int received = await stream.ReadAsync(buffer);
 
-var message = Encoding.UTF8.GetString(buffer, 0, received);
-Console.WriteLine($"Message received: \"{message}\"");
+//var message = Encoding.UTF8.GetString(buffer, 0, received);
+//Console.WriteLine($"Message received: \"{message}\"");
 
